c7212d2fa3284ce8732a7daadd34b5706a4806ce
ChandraPrakash1486 <chandraprakashtekwani8@gmail.com>
2026-03-04 21:47:08 +0530

    Delete hash_maker.py

--- app/hash_maker.py
+++
@@ -1,5 +0,0 @@
-from passlib.context import CryptContext
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-print("Admin Hash:", pwd_context.hash("iamadmin112"))
-print("Guest Hash:", pwd_context.hash("guest123"))
